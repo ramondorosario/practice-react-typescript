21812ad07f30d56c34a45b63a3a67fd6372657b5
Ramon do Rosário <ramondorosario@gmail.com>
2021-01-01 17:27:41 -0300

    feat: create route for LoginPage container

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+
+const LoginPage = React.lazy(() =>
+  import("../containers/LoginPage").then((res): any => ({
+    default: res.LoginPage,
+  }))
+);
 
 export function Routes() {
-  return <div>Hello world</div>;
+  return (
+    <Switch>
+      <Route exact path="/" render={() => <Redirect to="/login" />} />
+      <Route path="/login" component={LoginPage} />
+    </Switch>
+  );
 }
