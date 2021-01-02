16f9c431d6e1589555ba2b0c6cb7bdb344123ef9
Ramon do Rosário <ramondorosario@gmail.com>
2021-01-02 15:39:56 -0300

    feat: create new routes for Signup page and ForgotPassword page

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -2,8 +2,18 @@ import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 
 const LoginPage = React.lazy(() =>
-  import("../containers/LoginPage").then((res): any => ({
+  import("../containers/LoginPage").then((res) => ({
     default: res.LoginPage,
+  }))
+);
+const SignupPage = React.lazy(() =>
+  import("../containers/SignupPage").then((res) => ({
+    default: res.SignupPage,
+  }))
+);
+const ForgotPassword = React.lazy(() =>
+  import("../containers/ForgotPassword").then((res) => ({
+    default: res.ForgotPassword,
   }))
 );
 
@@ -12,6 +22,8 @@ export function Routes() {
     <Switch>
       <Route exact path="/" render={() => <Redirect to="/login" />} />
       <Route path="/login" component={LoginPage} />
+      <Route path="/signup" component={SignupPage} />
+      <Route path="/forgot-password" component={ForgotPassword} />
     </Switch>
   );
 }
