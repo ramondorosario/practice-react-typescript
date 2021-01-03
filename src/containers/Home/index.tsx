d4f7f1361d13602fc5c9e56c7b99340355cf7027
Ramon do Rosário <ramondorosario@gmail.com>
2021-01-03 13:54:52 -0300

    feat: add new container

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -11,59 +11,62 @@ export function Home() {
   return (
     <div className={s.container}>
       <SideBar />
-      <div className={s.content}>
+
+      <div className={s.body}>
         <Header />
-        <div className={s.content_menu}>
-          <ul>
-            <li
-              onClick={() => setMenuSelected("este mês")}
-              className={menuSelected === "este mês" ? s.selected : ""}
-            >
-              Este mês
-            </li>
-            <li
-              onClick={() => setMenuSelected("este ano")}
-              className={menuSelected === "este ano" ? s.selected : ""}
-            >
-              Este ano
-            </li>
-            <li
-              onClick={() => setMenuSelected("desde o início")}
-              className={menuSelected === "desde o início" ? s.selected : ""}
-            >
-              Desde o início
-            </li>
-          </ul>
-        </div>
-        <div className={s.cards}>
-          <div className={s.card}>
-            <header className={s.header}>
-              <IconCustomers />
-              <span>Clientes</span>
-            </header>
-            <div className={s.card_content}>
-              <div className={`${s.condition} ${s.in_day}`}>
-                Em dia <span className={s.value}>0</span>
-              </div>
-              <div className={`${s.condition} ${s.pending}`}>
-                Inadimplentes <span className={s.value}>0</span>
+        <div className={s.content}>
+          <div className={s.content_menu}>
+            <ul>
+              <li
+                onClick={() => setMenuSelected("este mês")}
+                className={menuSelected === "este mês" ? s.selected : ""}
+              >
+                Este mês
+              </li>
+              <li
+                onClick={() => setMenuSelected("este ano")}
+                className={menuSelected === "este ano" ? s.selected : ""}
+              >
+                Este ano
+              </li>
+              <li
+                onClick={() => setMenuSelected("desde o início")}
+                className={menuSelected === "desde o início" ? s.selected : ""}
+              >
+                Desde o início
+              </li>
+            </ul>
+          </div>
+          <div className={s.cards}>
+            <div className={s.card}>
+              <header className={s.header}>
+                <IconCustomers />
+                <span>Clientes</span>
+              </header>
+              <div className={s.card_content}>
+                <div className={`${s.condition} ${s.in_day}`}>
+                  Em dia <span className={s.value}>0</span>
+                </div>
+                <div className={`${s.condition} ${s.pending}`}>
+                  Inadimplentes <span className={s.value}>0</span>
+                </div>
               </div>
             </div>
-          </div>
-          <div className={s.card}>
-            <header className={s.header}>
-              <IconChanges />
-              <span>Cobranças</span>
-            </header>
-            <div className={s.card_content}>
-              <div className={`${s.condition} ${s.expected}`}>
-                Previstas <span className={s.value}>0</span>
-              </div>
-              <div className={`${s.condition} ${s.pending}`}>
-                Vencidas <span className={s.value}>0</span>
-              </div>
-              <div className={`${s.condition} ${s.in_day}`}>
-                Pagas <span className={s.value}>0</span>
+            <div className={s.card}>
+              <header className={s.header}>
+                <IconChanges />
+                <span>Cobranças</span>
+              </header>
+              <div className={s.card_content}>
+                <div className={`${s.condition} ${s.expected}`}>
+                  Previstas <span className={s.value}>0</span>
+                </div>
+                <div className={`${s.condition} ${s.pending}`}>
+                  Vencidas <span className={s.value}>0</span>
+                </div>
+                <div className={`${s.condition} ${s.in_day}`}>
+                  Pagas <span className={s.value}>0</span>
+                </div>
               </div>
             </div>
           </div>
